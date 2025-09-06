06d8660d7b5c297749bb2b118950d80ecb16fbbf
DTalis <dtalis92@gmail.com>
2025-09-06 13:58:37 +0300

    comment

---
+++ Py/DI-Bootcamp/Week1/Day5/DailyChalenge/Challenges.py
@@ -0,0 +1,30 @@
+
+print("\n🌟Challenge 1: Sorting")
+my_list = input("Enter words separated by commas: ")
+print(f"Your list is: {my_list}")
+
+words = my_list.split(',')
+
+words.sort()
+
+sorted_string = ','.join(words)
+
+print(sorted_string)
+
+
+print("\n🌟Challenge 2: Longest Word")
+def longest_word(sentence):
+    each_word = sentence.split()
+    longest = ""
+    max_length = 0
+    for word in each_word:
+        if len(word) > max_length:
+            longest = word
+            max_length = len(word)
+    return longest
+
+# Get sentence input from the user
+user_sentence = input("Enter a sentence: ")
+
+# Find and print the longest word
+print(longest_word(user_sentence))
